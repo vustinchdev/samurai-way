a2abbcc2a9799a3659f5a4c4edd64467c938855b
vustinchdev <vustinchdev@gmail.com>
2023-10-11 16:15:53 +0300

    added new components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import s from './Navbar.module.css'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <ul>
                 <li className={s.item}>
-                    <a>Profile</a>
-                </li>
-                <li className={`${s.item} ${s.active}`}>
-                    <a>Messages</a>
+                    <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
                 </li>
                 <li className={s.item}>
-                    <a>News</a>
+                    <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
                 </li>
                 <li className={s.item}>
-                    <a>Music</a>
+                    <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to='music' activeClassName={s.activeLink}>Music</NavLink>
+                </li>
+                <li className={s.item}>
+                    <NavLink to='settings' activeClassName={s.activeLink}>Settings</NavLink>
                 </li>
             </ul>
         </nav>
