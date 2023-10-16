0dd451a2f7332b75440d17416ff9afaebd80dbcf
vustinchdev <vustinchdev@gmail.com>
2023-10-16 14:19:46 +0300

    changes in Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Dima</div>
+                <div className={s.dialog}>Petr</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Alex</div>
+                <div className={s.dialog}>Valera</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you?</div>
+            </div>
         </div>
     )
 }
