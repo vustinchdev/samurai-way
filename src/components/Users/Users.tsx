2735df4da87c5eec859c2f55ff44959b0e34a6f7
vustinchdev <vustinchdev@gmail.com>
2023-12-12 14:40:42 +0300

    componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,8 +7,7 @@ import { UsersType } from './UsersContainer'
 
 export class Users extends React.Component<UsersType> {
 
-    constructor(props: UsersType) {
-        super(props)
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => this.props.setUsers(res.data.items))
     }
