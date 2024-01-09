3c06b9959df3350310d09a7bc200e21d05c5c575
vustinchdev <vustinchdev@gmail.com>
2024-01-09 10:08:46 +0300

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -32,6 +32,14 @@ export class ProfileStatus extends React.Component<ProfileStatusType> {
         })
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusType>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
