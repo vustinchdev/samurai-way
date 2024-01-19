e4e5ba6f9c88647452378c8ea8ea76369517a1c1
vustinchdev <vustinchdev@gmail.com>
2024-01-19 19:34:17 +0300

    add ProfileStatusWithHooks

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import s from './ProfileInfo.module.css'
-import { Preloader } from '../../common/Preloader/Preloader'
-import { ProfilePropsType } from '../Profile'
-import { ProfileStatus } from './ProfileStatus'
-
+import React from "react";
+import s from "./ProfileInfo.module.css";
+import { Preloader } from "../../common/Preloader/Preloader";
+import { ProfilePropsType } from "../Profile";
+import { ProfileStatusWithHooks } from "./ProfileStatusWithHooks";
 
 export const ProfileInfo: React.FC<ProfilePropsType> = (props) => {
-    if (!props.profile) {
-        return <Preloader />
-    }
+  if (!props.profile) {
+    return <Preloader />;
+  }
 
-    return (
-        <div>
-            {/* <div>
+  return (
+    <div>
+      {/* <div>
                 <img src="https://images.unsplash.com/photo-1610878180933-123728745d22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FuYWRhJTIwbmF0dXJlfGVufDB8fDB8fHww&w=1000&q=80" alt="" />
             </div> */}
-            <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                <div>{props.profile.fullName}</div>
-                <div>{props.profile.aboutMe}</div>
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
-            </div>
-        </div>
-    )
-}
+      <div className={s.descriptionBlock}>
+        <img src={props.profile.photos.large} />
+        <div>{props.profile.fullName}</div>
+        <div>{props.profile.aboutMe}</div>
+        <ProfileStatusWithHooks
+          status={props.status}
+          updateStatus={props.updateStatus}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent, useState } from "react";
+
+type ProfileStatusType = {
+  status: string;
+  updateStatus: (status: string) => void;
+};
+
+export const ProfileStatusWithHooks: React.FC<ProfileStatusType> = (props) => {
+  const [editMode, setEditMode] = useState(false);
+  const [status, setStatus] = useState(props.status);
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
+
+  const diactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatus(status);
+  };
+
+  const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setStatus(e.currentTarget.value);
+  };
+
+  return (
+    <div>
+      {!editMode && (
+        <div>
+          <span onDoubleClick={activateEditMode}>
+            {props.status || "-----"}
+          </span>
+        </div>
+      )}
+      {editMode && (
+        <div>
+          <input
+            value={status}
+            onChange={onStatusChange}
+            autoFocus={true}
+            onBlur={diactivateEditMode}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
