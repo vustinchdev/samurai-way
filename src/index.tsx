1e562bc207feda670dfbfc0c3345a27facb6cd0c
vustinchdev <vustinchdev@gmail.com>
2024-02-09 19:30:10 +0300

    change styles for button

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/redux-store";
 import { Provider } from "react-redux";
 import "antd/dist/antd.css";
+import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
